d5135f89182e98cffcc9bb64bb8ef7a94870e7ed
Matthew Citron <matthewdc2@gmail.com>
2016-11-04 17:15:39 +0000

    app doc folder

---
+++ cv/run.sh
@@ -0,0 +1,15 @@
+pdflatex elwood.tex
+bibtex pub.aux
+bibtex coord.aux
+bibtex review.aux
+bibtex prelim.aux
+bibtex thesis.aux
+bibtex proc.aux
+bibtex conf.aux
+bibtex talks.aux
+pdflatex elwood.tex
+pdflatex elwood.tex
+open elwood.pdf
+
+#pdflatex covering_letter.tex
+#preview covering_letter.pdf
