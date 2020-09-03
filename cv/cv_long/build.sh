a236f5354947bfbb17fa0a1aa98e10f2bc85bfe7
Matthew Citron <mcitron@cern.ch>
2020-09-03 11:18:12 -0700

    added KARP

--- cv/cv_long/build.sh
+++ cv/cv_long/build.sh
@@ -1,2 +1,3 @@
-pdflatex citron_CV.tex && bibtex citron_CV && pdflatex citron_CV.tex && pdflatex citron_CV.tex
-pdflatex cover_letter_VUB.tex
+# pdflatex citron_CV.tex && bibtex citron_CV && pdflatex citron_CV.tex && pdflatex citron_CV.tex
+# pdflatex cover_letter_VUB.tex
+xelatex citron_publist.tex && bibtex citron_publist && xelatex citron_publist.tex && xelatex citron_publist.tex
